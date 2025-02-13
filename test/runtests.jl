7383a19a11a4c1be327ad30768f00fdc51bcb4bf
Éric Thiébaut <eric.thiebaut@univ-lyon1.fr>
2025-02-13 21:15:58 +0100

    Add tests

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,6 +1,27 @@
 using Backport
+@backport
 using Test
 
 @testset "Backport.jl" begin
-    # Write your tests here.
+    @testset "mapreduce" begin
+        @test mapreduce(*, +, (1,2), (3,4)) === 11
+        @test mapreduce(+, *, (1,2), (3,4)) === 24
+        @test mapreduce(abs, +, (-1, 2, 3, -4)) === 10
+    end
+    @testset "Memory{T}" begin
+        v = @inferred Memory{UInt8}(undef, 7)
+        @test v isa Memory{UInt8}
+        @test v isa AbstractVector{UInt8}
+        @test v isa DenseVector{UInt8}
+        @test length(v) == 7
+        @test keys(v) == 1:7
+    end
+    @testset "Returns" begin
+        f = @inferred Returns(pi)
+        @test f() === pi
+        @test f(1,"e"; a=9) === pi
+        f = @inferred Returns{Float32}(pi)
+        @test f() === Float32(pi)
+        @test f(; c=:foo, d=1) === Float32(pi)
+    end
 end
